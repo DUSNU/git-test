20ba0247eda190ca3d9d4db9bf325881176dc43b
Dusnu <1273896333@qq.com>
2022-02-16 16:08:17 +0800

    first commit

---
+++ src/main/java/com/kemo/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.kemo.git;
+
+/**
+ * @author kemo
+ * @create 2022-02-16 16:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
